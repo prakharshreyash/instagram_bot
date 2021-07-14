dc90fc7b0b93bd57094fb13c0e147f0113474540
Prakhar Shreyash <64785098+prakharshreyash15@users.noreply.github.com>
2021-07-14 09:15:19 +0530

    Source Code

--- insta_bot.py
+++ insta_bot.py
@@ -1,10 +1,10 @@
 from instapy import InstaPy
 
-session = InstaPy(username = "tanay_s15", password = "prakharsingh")
+session = InstaPy(username = "<your username>", password = "<your password>")
 session.login()
 
 session.set_relationship_bounds(enabled = true, max_followers = 200)
 
-session.set_do_follow(True, percentage = 10)
+session.set_do_follow(True, percentage = 100)
 
 session.end()
